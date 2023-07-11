bc2c336195c7b5523dfd15537370c126a184314a
homvi <honvedoadamkj@gmail.com>
2023-07-11 11:59:52 +0200

    custumize hero

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ function App() {
   return (
 <>
 <Hero />
-<Hero />
 </>
   )
   
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,18 +1,17 @@
 const Hero = () => {
   return (
     //Photo by <a href="https://unsplash.com/@iriser?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Irina Iriser</a> on <a href="https://unsplash.com/wallpapers/nature/forest?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-    <div className="hero min-h-screen bg-base-200 bg-hero-pattern">
-      <div className="hero-overlay bg-opacity-50"></div>
-      <div className="hero-content flex-col lg:flex-row-reverse  bg-gradient-to-r from-black/30 to-black/0 rounded-2xl">
-        <div className="w-screen">
-          <h1 className="text-5xl text-white font-bold">Isten hozta önöket!</h1>
+    <div className="flex  items-center  justify-start  w-full bg-cover bg-center  h-screen bg-hero-pattern">
+      <div className=" md:ml-10 w-full p-5 h-full md:h-fit flex flex-col justify-center bg-gradient-to-r from-black/50 to-black/0 rounded-md">
+        <div className="w-full">
+          <h1 className="text-4xl text-white font-bold">Isten hozta önöket!</h1>
           <p className="py-6 text-white max-w-xl ">
             Megtiszteltetés számomra, hogy a település polgármestereként,
             önkormányzatunk nevében köszönthetek minden érdeklődőt. Remélem, a
             honlap böngészése után kedvet érez majd, hogy személyesen is
             megismerkedjen településünkkel és az itt élő emberekkel!
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary">Tovább a hírekhez</button>
         </div>
       </div>
     </div>
