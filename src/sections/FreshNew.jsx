31d4d1cb5d09b11e484a30d6230abbd8922aefe4
homvi <honvedoadamkj@gmail.com>
2023-07-19 09:01:43 +0200

    add static image

--- src/sections/FreshNew.jsx
+++ src/sections/FreshNew.jsx
@@ -1,6 +1,8 @@
+import erzsebet from "../assets/img/erzsebet.jpg";
+
 const freshNew = {
   title: "Erzsébet Tábor Plusz 2023",
-  img: "https://scontent.fbcn13-1.fna.fbcdn.net/v/t39.30808-6/358410015_281821454507316_525263802385808858_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=_s_KIKH-r1UAX8DL7io&_nc_ht=scontent.fbcn13-1.fna&oh=00_AfAePsOt1IB1MdPpLJ95Fj27N3_gVPRp_C-YjiFBQtU2_w&oe=64B24891",
+  img: erzsebet,
   description: "(Erzsébet Tábor-Tábordal)",
   quote:
     '"Gyere el, kérünk, légy a vendégünk! S lesz egy szép, közösemlékünk..."',
@@ -13,12 +15,12 @@ const FreshNew = () => {
     <div id="news" className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row">
         <img
-        alt={freshNew.title}
+          alt={freshNew.title}
           src={freshNew.img}
           className="max-w-screen  rounded-lg shadow-2xl"
         />
         <div>
-            <div className="text-slate-600">{freshNew.date}</div>
+          <div className="text-slate-600">{freshNew.date}</div>
           <h1 className="text-5xl font-bold">{freshNew.title}</h1>
           <p className="py-6 prose prose-stone  ">
             {freshNew.quote && (
